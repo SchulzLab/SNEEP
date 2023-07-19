6b33464497d4456a931ca1f69573a0df80d83ea0
BaumgartenNina <46556394+BaumgartenNina@users.noreply.github.com>
2023-07-19 17:22:35 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SNEEP'
+copyright = '2023, SchulzLab'
+author = 'Baumgarten'
 
 release = '0.1'
 version = '0.1.0'
